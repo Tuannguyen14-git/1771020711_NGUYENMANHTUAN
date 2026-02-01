44db7382b7ec32e01cf3b2ca1aa7469ee0560deb
NGUYEN MANH TUAN <tn140000@gmail.com>
2026-02-01 15:57:37 +0700

    Remove conflicting Vercel workflow and focus on Render deployment

--- Pcm.Api/Pcm.Api/obj/Debug/net8.0/Pcm.Api.AssemblyInfo.cs
+++ Pcm.Api/Pcm.Api/obj/Debug/net8.0/Pcm.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pcm.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a112ab0b6a578a99d8e9bf8df1aeb9df70fbf5ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b600a2c84d1d9a87b061cfa91c33375108ebbf70")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pcm.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pcm.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
